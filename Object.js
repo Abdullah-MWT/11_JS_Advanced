39bf3f794bf2522a6f8e3d5c9b8c71bc257886d9
Abdullah-MWT <abdullahdeveloper2018@gmail.com>
2024-08-20 23:57:15 +0500

    My 11th JS Code Folder

---
+++ Object.js
@@ -0,0 +1,12 @@
+// NOTE:- This Folder is created after ProtoTypes.js, So make sure, you first Check that 
+
+// +++++++++++++  Objects in Detail +++++++++++++++++
+
+function createFullName(firstName,lastName) {
+     this.firstName = firstName;
+     this.lastName = lastName;
+}
+
+createFullName.prototype.addNickName = function() {
+    this.lastName + addNickName
+}
--- ProtoTypes.js
+++ ProtoTypes.js
@@ -1,1 +1,30 @@
-console.log("Comming Soon");
+// JavaScript has ProtoTypal behavior beacuase in JavaScript, objects can inherit directly from other objects, and this inheritance is dynamic.
+
+// Create a basic object to UnderStand the ProtoTypal Behavior of Js
+let Human = {
+  skinColor: "White",
+  behavior: "Best",
+  isStudent: true,
+};
+
+let Abdullah = Object.create(Human);
+let Zaryab = Object.create(Human);
+Abdullah.isTeacher = false;
+Zaryab.isFrom = "S.K";
+
+console.log(Abdullah.behavior);
+console.log(Zaryab.isStudent);
+
+// Here, Abdullah & Zaryab don,t have a direct properties called .behavior & .isStudent, but they inherit it from Human because they are created with Human as its prototype.
+
+// ++++++++++ Let,s Prove that function is an Object In reality in JS
+function addFiveToNum(num) {
+  return num + 5;
+}
+addFiveToNum.added = 5;
+
+console.log(addFiveToNum.added); // Now , as we Accessed the the just above value thorugh dot' . ' , which is Objects Characteristic, Therefore we Can Say that Function is also an Object
+
+console.log(addFiveToNum(7));
+console.log(addFiveToNum.prototype);  // OutPut = {}, it means it,s an object and has some protoTypes
+
